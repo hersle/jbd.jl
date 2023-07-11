8fe403055591f0b65e06d38c99db3610df65fe8e
Herman Sletmoen <hermansletmoen@gmail.com>
2023-07-11 17:35:57 +0200

    Change to physical density parameters

--- background.jl
+++ background.jl
@@ -4,12 +4,10 @@ using DifferentialEquations
 using Printf
 
 struct Parameters
-    # TODO: what if I give physical parameters instead???
-    # TODO: how is h a derived parameter?
     ω::Float64   # Brans-Dicke coupling
-    Ωr0::Float64 # "unphysical" radiation   density parameter
-    Ωm0::Float64 # "unphysical" matter      density parameter
-    ΩΛ0::Float64 # "unphysical" dark energy density parameter
+    ωr0::Float64 # physical radiation   density parameter (= Ωr0*h0^2)
+    ωm0::Float64 # physical matter      density parameter (= Ωm0*h0^2)
+    ωΛ0::Float64 # physical dark energy density parameter (= ΩΛ0*h0^2)
 end
 
 function fixed_point_iterate(f::Function, x0; maxiters=100)
@@ -23,33 +21,33 @@ function fixed_point_iterate(f::Function, x0; maxiters=100)
     return NaN # did not converge within maxiters
 end
 
-
 a(x) = exp(x) # x = log(a)
 
 # ODE system to solve:
-#   y[1] = logϕ
-#   y[2] = a^3 * E * ϕ * dlogϕ/dloga
-#   dy_dx[1] = dlogϕ / dloga
-#   dy_dx[2] = d(a^3*E*ϕ*dlogϕ/dloga) / dloga = 3 * a^3 * (Ωm0/a^3 + 4*ΩΛ0) / ((3 + 2ω) * E)
+#     y[1] = logϕ
+#     y[2] = a^3 * h * ϕ * dlogϕ/dloga
+#     dy_dx[1] = dlogϕ / dloga
+#     dy_dx[2] = d(a^3*h*ϕ*dlogϕ/dloga) / dloga = 3 * a^3 * (ωm0/a^3 + 4*ωΛ0) / ((3 + 2ω) * h)
+# (h(t) = H(t) / (100 km/(s*Mpc)) is *time-dependent* reduced Hubble parameter, to make notation consistent)
 function integrate_ϕ(θ::Parameters, ϕini; aini=1e-10)
-    dy_dx(y, _, x) = [dlogϕ_dloga(θ, y, x), 3 / (3+2*θ.ω) * a(x)^3 * (θ.Ωm0/a(x)^3 + 4*θ.ΩΛ0) / E(θ, y, x)] # dy_dx[1], dy_dx[2]
-    return solve(ODEProblem(dy_dx, [log(ϕini), 0.0], (log(aini), 0.0)))
+    dy_dx(y, _, x) = [dlogϕ_dloga(θ, y, x), 3 / (3+2*θ.ω) * a(x)^3 * (θ.ωm0/a(x)^3 + 4*θ.ωΛ0) / h(θ, y, x)] # dy_dx[1], dy_dx[2]
+    return solve(ODEProblem(dy_dx, [log(ϕini), 0.0], (log(aini), 0.0))) # assumption: dlogϕ_dloga = 0 at a=aini
 end
 
 # relevant quantities as function of parameters and ODE state:
        logϕ(θ::Parameters, y, x) = y[1]
           ϕ(θ::Parameters, y, x) = exp(logϕ(θ, y, x))
-dlogϕ_dloga(θ::Parameters, y, x) = fixed_point_iterate(y1′ -> y[2]*(1 + y1′ - θ.ω/6*(y1′)^2) / (θ.Ωr0/a(x)^4 + θ.Ωm0/a(x)^3 + θ.ΩΛ0) / a(x)^3, 0.0) # solve y[2] = a^3 * (E*ϕ) * dlogϕ_dloga for y1′ = dlogϕ_dloga, with the expression for E expanded, using fixed-point iteration
-          E(θ::Parameters, y, x) = √( 1/ϕ(θ, y, x) * (θ.Ωr0/a(x)^4 + θ.Ωm0/a(x)^3 + θ.ΩΛ0) / (1 + dlogϕ_dloga(θ, y, x) - θ.ω/6*(dlogϕ_dloga(θ, y, x))^2) )
+dlogϕ_dloga(θ::Parameters, y, x) = fixed_point_iterate(y1′ -> y[2]*(1 + y1′ - θ.ω/6*(y1′)^2) / (θ.ωr0/a(x)^4 + θ.ωm0/a(x)^3 + θ.ωΛ0) / a(x)^3, 0.0) # solve y[2] = a^3 * (h*ϕ) * dlogϕ_dloga for y1′ = dlogϕ_dloga, with the expression for h expanded, using fixed-point iteration
+          h(θ::Parameters, y, x) = √( 1/ϕ(θ, y, x) * (θ.ωr0/a(x)^4 + θ.ωm0/a(x)^3 + θ.ωΛ0) / (1 + dlogϕ_dloga(θ, y, x) - θ.ω/6*(dlogϕ_dloga(θ, y, x))^2) )
 
-# find ϕini and ΩΛ0 such that ϕ0=1 and E0=1
-function shoot(ω=1e2, Ωr0=5.5e-5, Ωm0=0.317, ϕ0_target = 1.0; maxiters=100, verbose=false)
+# find ϕini and ωΛ0 such that ϕ0=1
+function shoot(ω=1e4, ωr0=5.5e-5*0.67^2, ωm0=0.317*0.67^2, ϕ0_target = 1.0; maxiters=100, verbose=false)
     @assert ϕ0_target > 0.0
     ϕini_lo = 0.0       # lower bound, guaranteed to have ϕ0 (= 0.0) < ϕ0_target
     ϕini_hi = ϕ0_target # upper bound, guaranteed to have ϕ0         > ϕ0_target
 
-    ΩΛ0 = 1 - Ωr0 - Ωm0 # initial *guess* for ΩΛ0 (based on sum(Ωi0)=1 and Ωϕ0≈0)
-    θ = Parameters(ω, Ωr0, Ωm0, ΩΛ0)
+    ωΛ0 = 0.0 # extremely stupid initial *guess* (don't have any value of h0 to use)
+    θ = Parameters(ω, ωr0, ωm0, ωΛ0)
 
     for iter in 1:maxiters
         # integrate scalar field with current cosmological parameters
@@ -60,28 +58,30 @@ function shoot(ω=1e2, Ωr0=5.5e-5, Ωm0=0.317, ϕ0_target = 1.0; maxiters=100, 
         x0 = 0.0
         y0 = y(x0)
         ϕ0 = ϕ(θ, y0, x0)
-        E0 = E(θ, y0, x0)
-        Ωϕ0 = -dlogϕ_dloga(θ, y0, x0) + θ.ω/6 * dlogϕ_dloga(θ, y0, x0)^2
-        Ω0 = Ωr0 + Ωm0 + ΩΛ0 + Ωϕ0
+        h0 = h(θ, y0, x0)
+        ωϕ0 = -dlogϕ_dloga(θ, y0, x0) + θ.ω/6 * dlogϕ_dloga(θ, y0, x0)^2
 
         if verbose
-            @printf "#%02d: JBD with ϕini = %.10f, ΩΛ0 = %.10f has ϕ0 = %.10f, E0 = %.10f, Ωϕ0 = %.10f, ∑(Ωi0) = %.10f\n" iter ϕini θ.ΩΛ0 ϕ0 E0 Ωϕ0 Ω0
+            @printf "#%02d: JBD with ϕini = %.10f, ωΛ0 = %.10f has ϕ0 = %.10f, h0 = %.10f, ωϕ0 = %.10f\n" iter ϕini θ.ωΛ0 ϕ0 h0 ωϕ0
         end
 
-        # we want the cosmology with (ϕini, ΩΛ0) such that ϕ0 = 1 and E0 = 1
-        if ϕini_lo ≈ ϕini_hi && E0 ≈ 1.0 && ϕ0 ≈ 1.0
+        # we want the cosmology with (ϕini, ωΛ0) such that ϕ0 = 1
+        if ϕini_lo ≈ ϕini_hi && ϕ0 ≈ 1.0
             return θ, ϕini # converged within maxiters
         end
 
-        # update ΩΛ0 (so next cosmology has E closer to 1)
-        ΩΛ0 = 1 - θ.Ωm0 - θ.Ωr0 - Ωϕ0 # equivalent to E = 1
-        θ = Parameters(θ.ω, θ.Ωr0, θ.Ωm0, ΩΛ0)
+        # update ωΛ0 to the value that satisfies the closure condition ∑(Ωi0) = 1 for the h0 in this cosmology
+        # (then we must reiterate to correct the scalar field integral, which depends on ωΛ0)
+        Ωr0, Ωm0, Ωϕ0 = ωr0/h0^2, ωm0/h0^2, ωϕ0/h0^2
+        ΩΛ0 = 1 - Ωr0 - Ωm0 - Ωϕ0 # closure condition; equivalent to E = 1
+        ωΛ0 = ΩΛ0*h0^2
+        θ = Parameters(θ.ω, θ.ωr0, θ.ωm0, ωΛ0)
 
-        # update ϕ0 (adjust aim) and shoot again
-        if ϕ0 > ϕ0_target
-            ϕini_hi = ϕini # hit above, so need lower  ϕini
-        else
-            ϕini_lo = ϕini # hit below, so need higher ϕini
+        # update ϕ0 (adjust aim), then shoot again
+        if ϕ0 > ϕ0_target  # hit above,
+            ϕini_hi = ϕini # so need lower  ϕini
+        else               # hit below,
+            ϕini_lo = ϕini # so need higher ϕini
         end
     end
 
