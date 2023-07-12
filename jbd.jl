7b834553f43911f7e3d28d06b54b903148e7cce1
Herman Sletmoen <hermansletmoen@gmail.com>
2023-07-12 15:17:43 +0200

    Cleanup

--- jbd.jl
+++ jbd.jl
@@ -1,8 +1,5 @@
 module JBD
 
-# 1) Give unphysical Ωi0, h -> ΩΛ0
-# 2) Give   physical ωi0    -> ΩΛ0, h derived
-
 using DifferentialEquations
 using Printf
 
@@ -14,6 +11,7 @@ struct Parameters
     ωΛ0::Float64  # physical dark energy density parameter (= ΩΛ0*h0^2)
 end
 
+# find two roots (x1, x2) of quadratic equation a*x^2 + b*x + c*x = 0
 function quadroots(a, b, c)
     d = b^2 - 4*a*c
     x1 = d >= 0.0 ? (-b + √(d)) / (2*a) : NaN
@@ -30,29 +28,28 @@ a(x) = exp(x) # x = log(a)
 #   y2′ = d(a^3*h*ϕ*dlogϕ/dloga) / dloga = 3 * a^3 * (ωm0/a^3 + 4*ωΛ0) / ((3 + 2ω) * h),
 # where h(t)^2 = (H(t) / (100 km/(s*Mpc)))^2 = 1/ϕ * (ωr0/a^4 + ωm0/a^3 + ωΛ0) / (1 + dlogϕ/dloga - ω/6 * (dlogϕ/dloga)^2)
 # is *time-dependent* reduced Hubble parameter (to make notation consistent)
+
+       logϕ(θ::Parameters, y, x) = y[1]
+          ϕ(θ::Parameters, y, x) = exp(logϕ(θ, y, x))
+dlogϕ_dloga(θ::Parameters, y, x) = quadroots(a(x)^6 * ϕ(θ,y,x) * (θ.ωr0/a(x)^4 + θ.ωm0/a(x)^3 + θ.ωΛ0) + θ.ω/6 * y[2]^2, -y[2]^2, -y[2]^2)[1] # positive solution y1′=dlogϕ/dloga of quadratic equation y1′ = y2/(a^3*h(y1′)*ϕ) with h(y1′) expanded
+          h(θ::Parameters, y, x) = √( 1/ϕ(θ, y, x) * (θ.ωr0/a(x)^4 + θ.ωm0/a(x)^3 + θ.ωΛ0) / (1 + dlogϕ_dloga(θ, y, x) - θ.ω/6 * (dlogϕ_dloga(θ, y, x))^2) )
+
 function integrate_ϕ(θ::Parameters; aini=1e-10)
     y1′(y, x) = dlogϕ_dloga(θ, y, x)
     y2′(y, x) = 3 * a(x)^3 / (3+2*θ.ω) * (θ.ωm0/a(x)^3 + 4*θ.ωΛ0) / h(θ, y, x)
     y′(y, _, x) = [y1′(y, x), y2′(y, x)]
     y0 = [log(θ.ϕini), 0.0] # assumption: dlogϕ_dloga = 0 (so y2′ = 0) at early a = aini
     x0 = log(aini)
-    return solve(ODEProblem(y′, y0, (x0, 0.0)))
+    return solve(ODEProblem(y′, y0, (x0, 0.0)), abstol=1e-6, reltol=1e-6)
 end
 
-# relevant quantities as function of parameters and ODE state:
-       logϕ(θ::Parameters, y, x) = y[1]
-          ϕ(θ::Parameters, y, x) = exp(logϕ(θ, y, x))
-dlogϕ_dloga(θ::Parameters, y, x) = quadroots(a(x)^6 * ϕ(θ,y,x) * (θ.ωr0/a(x)^4 + θ.ωm0/a(x)^3 + θ.ωΛ0) + θ.ω/6 * y[2]^2, -y[2]^2, -y[2]^2)[1] # positive solution y1′=dlogϕ/dloga of quadratic equation y1′ = y2/(a^3*h(y1′)*ϕ) with h(y1′) expanded
-          h(θ::Parameters, y, x) = √( 1/ϕ(θ, y, x) * (θ.ωr0/a(x)^4 + θ.ωm0/a(x)^3 + θ.ωΛ0) / (1 + dlogϕ_dloga(θ, y, x) - θ.ω/6 * (dlogϕ_dloga(θ, y, x))^2) )
-
 # find ϕini and ωΛ0 such that ϕ0 = 1 and Ω0 = 1
-function shoot(ω=1e3, ωr0=5.5e-5*0.67^2, ωm0=0.317*0.67^2, ϕ0_target = 1.0; maxiters=100, verbose=false)
+function shoot(ω=1e3, ωr0=5.5e-5*0.67^2, ωm0=0.317*0.67^2; maxiters=100, verbose=false)
     println("Fixing ω = $ω, ωr0 = $ωr0, ωm0 = $ωm0;")
     println("Varying ωΛ0, ϕini until ϕ0 = 1 and Ω0 = 1:")
 
-    @assert ϕ0_target > 0.0
-    ϕini_lo = 0.0       # lower bound, guaranteed to have ϕ0 < ϕ0_target (since it gives ϕ0 = 0)
-    ϕini_hi = ϕ0_target # upper bound, guaranteed to have ϕ0 > ϕ0_target (since dlogϕ/dloga > 0)
+    ϕini_lo = 0.0 # lower bound, guaranteed to have ϕ0 < 1.0 (since it gives ϕ0 = 0)
+    ϕini_hi = 1.0 # upper bound, guaranteed to have ϕ0 > 1.0 (since dlogϕ/dloga > 0)
 
     ωΛ0 = 0.0 # extremely stupid initial *guess*; will be refined by the loop
 
@@ -91,7 +88,7 @@ function shoot(ω=1e3, ωr0=5.5e-5*0.67^2, ωm0=0.317*0.67^2, ϕ0_target = 1.0; 
         ωΛ0 = ΩΛ0*h0^2
 
         # update ϕ0 (adjust aim), then shoot again
-        if ϕ0 > ϕ0_target  # hit above,
+        if ϕ0 > 1.0  # hit above,
             ϕini_hi = ϕini # so need lower  ϕini
         else               # hit below,
             ϕini_lo = ϕini # so need higher ϕini
